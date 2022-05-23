ad8eaf66d0a68fb8cc4fc4864d469b65e2abc93c
quatbatieu <vuvantai894@gmail.com>
2022-05-23 19:45:56 +0700

    ad

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
-function baitap1(KetQua) {
+function baitap1(ketQua) {
   // B1: Xác định đầu vào: input số nguyên
   var n = document.getElementById("txtNum").value;
-  var number = soNT(n);
   // B2: Kiểm tra n có phải là số nguyên tố không
   if (n < 2) {
     alert("n không phải là số nguyên tố");
     return;
   }
   for(var j = 2; j < n ; j++){
-      if(n % j !== 0){
-        
+      if(soNT(j) === true){
+        ketQua += j + " ";
       }
   }
-//   var KetQua;
-//   for (var i = 2; i < n; i++) {
-//     if (n % i === 0) {
-//       KetQua = false;
-//       break;
-//     }
-//     if (KetQua === true) {
-//       KetQua += KetQua;
-//     }
-//   }
-
   var notiDiv = document.getElementById("notibt");
   notiDiv.style.display = "block";
-  divNoti.innerHTML = KetQua;
+  notiDiv.innerHTML = ketQua;
 }
 function soNT(n){
-  var KetQua;
+  var ketQua;
+  if(n<2){
+    return false  
+  }
+  if(n === 2){
+    return true
+  }
   for (var i = 2; i < n; i++) {
     if (n % i === 0) {
-      KetQua = false;
+      ketQua = false;
       break;
     } else {
-      KetQua = true;
+      ketQua = true;
     }
+    return ketQua
   }
-  return KetQua
 }
